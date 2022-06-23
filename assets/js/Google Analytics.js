2375c475861d8da5203eaaaad7450a8b7c7a387d
Adrian Herrmann <107512836+ah-java-dev@users.noreply.github.com>
2022-06-23 23:56:17 +0200

    Google Analytics update.

--- assets/js/Google Analytics.js
+++ assets/js/Google Analytics.js
@@ -1,7 +1,8 @@
 window.dataLayer = window.dataLayer || [];
+
 function gtag() {
     dataLayer.push(arguments);
 }
 
 gtag('js', new Date());
-gtag('config', 'G-2T72VBSPFK');
+gtag('config', 'UA-232872531-1');
